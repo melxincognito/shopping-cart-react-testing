641ace11018423bbe4ac21eb7fb9c8561f50a425
Mel Incognito <melxincognito@gmail.com>
2022-08-18 17:42:01 -0700

    finished most of the styling for page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,26 +16,30 @@ export default function HomePage() {
           );
         })}
       </div>
-      Home Page
+      <div className="circle"> MV Studio </div>
+      <div className="store-description">
+        {" "}
+        <h2> Shop for all your favorite clothes at MV Studio</h2>
+      </div>
     </div>
   );
 }
 
 const images = [
   {
-    img: "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
-    alt: "sneakers",
-  },
-  {
     img: "https://images.unsplash.com/photo-1603798125914-7b5d27789248?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
     alt: "shirt",
   },
   {
-    img: "https://images.unsplash.com/photo-1625093742435-6fa192b6fb10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1778&q=80",
-    alt: "makeup",
-  },
-  {
     img: "https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
     alt: "womens rack",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
+    alt: "sneakers",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1625093742435-6fa192b6fb10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1778&q=80",
+    alt: "makeup",
   },
 ];
