f4f579ce23b0959931861ab22d90be543d9d9d2c
Mel Incognito <melxincognito@gmail.com>
2022-08-18 19:13:12 -0700

    changed input number variable

--- src/pages/ShoppingPage.js
+++ src/pages/ShoppingPage.js
@@ -6,10 +6,43 @@ export default function ShoppingPage() {
     <div>
       <ShoppingPageItemCard
         itemLabel="Heels"
-        price="$4.00"
+        price={4.0}
         imageUrl="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80"
         imageAlt="Item 1"
       />
     </div>
   );
 }
+
+const items = [
+  {
+    label: "",
+    price: "$",
+    url: "",
+    alt: "",
+  },
+  {
+    label: "",
+    price: "$",
+    url: "",
+    alt: "",
+  },
+  {
+    label: "",
+    price: "$",
+    url: "",
+    alt: "",
+  },
+  {
+    label: "",
+    price: "$",
+    url: "",
+    alt: "",
+  },
+  {
+    label: "",
+    price: "$",
+    url: "",
+    alt: "",
+  },
+];
