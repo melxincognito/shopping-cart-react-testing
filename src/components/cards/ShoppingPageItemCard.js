6f5c26216eb9982421d8990d0c974212e1407798
Mel Incognito <melxincognito@gmail.com>
2022-08-18 18:09:49 -0700

    created card for shopping page items

---
+++ src/components/cards/ShoppingPageItemCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ShoppingPageItemCard.component.css";
+
+export default function ShoppingPageItemCard({
+  itemLabel,
+  price,
+  imageUrl,
+  imageAlt,
+  index,
+}) {
+  return (
+    <div className="cardContainer" id={itemLabel + " " + index}>
+      <img src={imageUrl} alt={imageAlt} />
+      <h2> {itemLabel}</h2>
+      <h3>{price}</h3>
+      <button onClick={() => alert(`${itemLabel} `)}> Add to cart</button>
+    </div>
+  );
+}
