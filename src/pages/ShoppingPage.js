886e7fada05d07b2c589c4bb335636b041198a69
Mel Incognito <melxincognito@gmail.com>
2022-08-18 18:09:16 -0700

    imported ShoppingPageItemCard

--- src/pages/ShoppingPage.js
+++ src/pages/ShoppingPage.js
@@ -1,5 +1,15 @@
 import React from "react";
+import ShoppingPageItemCard from "../components/cards/ShoppingPageItemCard";
 
 export default function ShoppingPage() {
-  return <div>Shopping Page</div>;
+  return (
+    <div>
+      <ShoppingPageItemCard
+        itemLabel="Heels"
+        price="$4.00"
+        imageUrl="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80"
+        imageAlt="Item 1"
+      />
+    </div>
+  );
 }
