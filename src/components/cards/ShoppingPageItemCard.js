0f30eb742e0e15c5221d4e29da49e9ec8718fde2
Mel Incognito <melxincognito@gmail.com>
2022-08-19 12:38:37 -0700

    changed image sizing

--- src/components/cards/ShoppingPageItemCard.js
+++ src/components/cards/ShoppingPageItemCard.js
@@ -6,16 +6,28 @@ export default function ShoppingPageItemCard({
   price,
   imageUrl,
   imageAlt,
+  index,
 }) {
+  const itemDetails = {
+    label: itemLabel,
+    price: price,
+    url: imageUrl,
+    alt: imageAlt,
+    id: index,
+  };
+
+  const sendToLocalStorage = () => {
+    window.localStorage.setItem(`${index}`, JSON.stringify(itemDetails));
+
+    alert("sent to local storage");
+  };
+
   return (
     <div className="cardContainer">
       <img src={imageUrl} alt={imageAlt} />
       <h2> {itemLabel}</h2>
       <h3>${price.toFixed(2)}</h3>
-      <button
-        onClick={() => alert(`${itemLabel}`)}
-        aria-label="add to cart button"
-      >
+      <button onClick={sendToLocalStorage} aria-label="add to cart button">
         {" "}
         Add to cart
       </button>
