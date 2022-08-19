9c95ad103867bf02915b759314ca59b90ea267f7
Mel Incognito <melxincognito@gmail.com>
2022-08-19 13:43:55 -0700

    added button functionality to increase and decrease quantity

--- src/components/cards/ShoppingCartItemCard.js
+++ src/components/cards/ShoppingCartItemCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ShoppingCartItemCard.component.css";
 
@@ -9,6 +9,20 @@ export default function ShoppingCartItemCard({
   imageAlt,
   index,
 }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    } else {
+      setQuantity(1);
+    }
+  };
+
   return (
     <div className="shoppingCartCard" id={index}>
       <div className="imageContainer">
@@ -20,9 +34,24 @@ export default function ShoppingCartItemCard({
           <h2>{title}</h2>
         </div>
         <div className="productCost">
-          <button>-</button>
-          <input type="text" value={1} />
-          <button>+</button>
+          <div className="productQuantity">
+            <button
+              onClick={decreaseQuantity}
+              className="quantityButton"
+              aria-label="decrease-quantity-button"
+            >
+              -
+            </button>
+            <input aria-label="quantity-input" type="text" value={quantity} />
+            <button
+              onClick={increaseQuantity}
+              className="quantityButton"
+              aria-label="increase-quantity-button"
+            >
+              +
+            </button>
+          </div>
+
           <h3>${price.toFixed(2)}</h3>
         </div>
       </div>
