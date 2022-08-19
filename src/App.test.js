254c8030c597342c53eb70ecfeb40929e65fefa9
Mel Incognito <melxincognito@gmail.com>
2022-08-18 19:14:31 -0700

    updated test for Shopping Cart Item Card

--- src/App.test.js
+++ src/App.test.js
@@ -112,7 +112,7 @@ describe("Shopping Cart Item Card", () => {
     render(
       <ShoppingPageItemCard
         itemLabel="Heels"
-        price="$4.00"
+        price={4}
         imageUrl="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80"
         imageAlt="Item 1"
       />
