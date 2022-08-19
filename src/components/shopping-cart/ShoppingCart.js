7138fe22b66b920659861f4f0afc838a504e6cb9
Mel Incognito <melxincognito@gmail.com>
2022-08-19 13:42:32 -0700

    imported shopping cart item card

--- src/components/shopping-cart/ShoppingCart.js
+++ src/components/shopping-cart/ShoppingCart.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Drawer, Button, Divider } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
+import ShoppingCartItemCard from "../cards/ShoppingCartItemCard";
+
 import "./ShoppingCart.component.css";
 
 export default function ShoppingCart() {
@@ -69,6 +71,13 @@ export default function ShoppingCart() {
       >
         <div className="mainContainer">
           <button onClick={() => console.log(itemsList)}>click me </button>
+          <ShoppingCartItemCard
+            title="heels"
+            price={400}
+            imageUrl="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80"
+            imageAlt="heels"
+            index={0}
+          />
         </div>
       </Drawer>
     </div>
